6734a66d3e67f84fb07d27fcf96e91da4ab6c32b
tometome2537 <146919125+tometome2537@users.noreply.github.com>
2025-03-31 00:43:57 +0900

    init

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            <h1>example-nextjs-app</h1>
+        </>
+    );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from "next";
+
+export default async function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="ja">
+            <head>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1, width=device-width"
+                />
+                <meta charSet="UTF-8" />
+            </head>
+            <body>{children}</body>
+        </html>
+    );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+    const title = "example-nextjs-app";
+    const description = "example-nextjs-app";
+
+    return {
+        title: title,
+        description: description,
+        openGraph: {
+            type: "website",
+            title: title,
+            description: description,
+            siteName: title,
+            url: "",
+            images: [
+                {
+                    url: "",
+                    width: 1600,
+                    height: 900,
+                    alt: title,
+                },
+            ],
+        },
+        twitter: {
+            card: "summary_large_image",
+            site: "@example",
+            title: title,
+            description: description,
+            images: [],
+        },
+    };
+}
